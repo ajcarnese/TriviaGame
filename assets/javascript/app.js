25f6b37104e282f38d263d6dcccb9abe230ced55
Tony Carnese <Tony Carnese>
2016-12-07 22:21:34 -0500

    added questions w/o javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// alert("hello!");
+
+var timeRemaining = 30;
+
+$("#time").append(timeRemaining)
+
+// $("#startbutton").on("click", function() {
+// 	$("#time").append("Time Remaining: " + timeRemaining)
+// });
